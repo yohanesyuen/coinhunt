29888c44dc3a0d27d5888b8e0f18d5d5bd79b6df
Yohanes Yuen <yuen.wei.ping@gmail.com>
2023-04-12 01:58:11 +0800

    Fix auto run celery

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ mkdir /app && cd /app
 git clone https://github.com/yohanesyuen/coinhunt.git
 cd /app/coinhunt
 pip install -e .
-celery -A coinhunt.tasks worker --loglevel=info
+# celery -A coinhunt.tasks worker --loglevel=info
 cp ./install/coinhunt.service /etc/systemd/system/coinhunt.service
 systemctl daemon-reload
 systemctl enable coinhunt.service

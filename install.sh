ae2ba29b2786087101f4d926102fd24c18a6a9f1
Yohanes Yuen <yuen.wei.ping@gmail.com>
2023-04-12 01:55:07 +0800

    Update install script

--- install.sh
+++ install.sh
@@ -3,6 +3,11 @@ apt update
 apt install python3-pip -y
 systemctl restart networkd-dispatcher.service unattended-upgrades.service
 mkdir /app && cd /app
-pip install -r requirements.txt
+git clone https://github.com/yohanesyuen/coinhunt.git
+cd /app/coinhunt
 pip install -e .
 celery -A coinhunt.tasks worker --loglevel=info
+cp ./install/coinhunt.service /etc/systemd/system/coinhunt.service
+systemctl daemon-reload
+systemctl enable coinhunt.service
+systemctl start coinhunt.service
--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
 from setuptools import setup, find_packages
 
+with open('requirements.txt', 'r') as fp:
+    requirements = fp.read().splitlines()
+
 setup(
     name='coinhunt',
     version='0.0.1',
     description='A Bitcoin private key finder',
-    packages=find_packages()
+    packages=find_packages(),
+    requires=requirements
 )
 

9c69212053308d9b57610d0f41c5ee0490a79a19
Yohanes Yuen <yuen.wei.ping@gmail.com>
2023-04-18 16:49:35 +0800

    Changes to install script

--- install.sh
+++ install.sh
@@ -1,11 +1,9 @@
-#/bin/sh
-apt update
-apt install python3-pip -y
+#/bin/bash
 systemctl restart networkd-dispatcher.service unattended-upgrades.service
-mkdir /app && cd /app
-git clone https://github.com/yohanesyuen/coinhunt.git
+mkdir /app
+git clone https://github.com/yohanesyuen/coinhunt.git /app/coinhunt
 cd /app/coinhunt
-pip install -e .
+pip install -e /app/coinhunt
 # celery -A coinhunt.tasks worker --loglevel=info
 cp ./install/coinhunt.service /etc/systemd/system/coinhunt.service
 systemctl daemon-reload

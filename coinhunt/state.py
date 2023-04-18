e39c7fedeab469862db9af8b657f82f8ad0ff328
Yohanes Yuen <yuen.wei.ping@gmail.com>
2023-04-18 16:48:38 +0800

    Made some changes to random searcher

--- coinhunt/state.py
+++ coinhunt/state.py
@@ -1,16 +1,21 @@
 from queue import Queue
 from typing import List, Tuple
 
+import heapq
 import json
 import os
 import random
 
 import logging
 
+import celery
 from coinhunt import ROOT_DIR
 from celery.result import AsyncResult
 
 logger = logging.getLogger()
+
+def get_workers():
+    return celery.current_app.control.inspect().ping()
 
 class State(object):
     def __init__(self):
@@ -18,17 +23,19 @@ class State(object):
         self.filename = os.path.join(ROOT_DIR, 'searched.json')
         if len(self.searched) == 0 and os.path.exists(self.filename):
             self.unmarshall()
-        self.max_length = 40
+        self.max_length = int(len(self.searched) * 1.5)
         self.ranges_being_searched = dict()
         self.ranges_queue = Queue()
         self.minimum = 2**65
         self.maximum = 2**66
         self.chunk_size = 60000
+        self.writes = 0
+        self.shrunken = False
 
         self.results: List[AsyncResult] = []
 
         self.compressing = False
-        self.trolled = True
+        self.trolled = False
 
         self.running = True
 
@@ -50,7 +57,6 @@ class State(object):
         return sorted(ranges, key=lambda x: x[0])
 
     def marshall(self):
-        self.searched = self.merge_searched_ranges(self.searched)
         with open(self.filename, "w") as f:
             json.dump(self.searched, f, indent=2)
 
@@ -60,7 +66,11 @@ class State(object):
 
     def update_searched_ranges(self, start, end):
         self.searched.append((start, end))
-        self.marshall()
+        self.writes += 1
+        self.searched = self.merge_searched_ranges(self.searched)
+        if self.writes > 10:
+            self.marshall()
+            self.writes = 0
 
     def compress_if_needed(self):
         if len(self.searched) > self.max_length and not self.compressing:
@@ -79,7 +89,23 @@ class State(object):
         for i in range(len(searched)-1):
             gap = searched[i+1][0] - searched[i][1]
             unsearched.append((gap, (searched[i][1], searched[i+1][0])))
-        return sorted(unsearched, key=lambda x: x[0])
+        res = sorted(unsearched, key=lambda x: x[0])
+        # res = [x for x in res if x[1][1] < self.maximum]
+        # res  = [x for x in res if x[1][0] > self.minimum]
+        # if res is None:
+        #     return []
+        return res
+
+    
+    def shrink_space(self):
+        unsearched = self.get_unsearched_ranges()
+
+        for r in unsearched:
+            if r[0] < self.chunk_size * 2:
+                continue
+            self.minimum = r[1][0]
+            self.maximum = r[1][1]
+        self.shrunken = True
 
     def get_random_range(self):
         self.compress_if_needed()
@@ -97,30 +123,33 @@ class State(object):
             end = self.maximum
             return self.limit_range(start, end)          
         # Get values between two ranges with the smallest gap if compressing
-        if len(searched) > 2:
-            if not self.ranges_queue.empty():
-                logger.debug('Getting values from ranges_queue')
+        if len(self.get_unsearched_ranges()) > 2:
+            if not self.trolled:
+                logger.debug('Getting values between two random ranges')
+                search_range = random.choice(self.get_unsearched_ranges())
+                if len(self.searched) > self.max_length:
+                    self.trolled = True
+            elif self.ranges_queue.qsize() > 0:
                 search_range = self.ranges_queue.get()
-            elif not self.trolled:
-                logger.debug('Chunkify gaps larger than 60k * 4')
-                for r in self.get_unsearched_ranges():
-                    if r[0] > self.chunk_size * 4:
-                        self.ranges_queue.put(r)
-                    if self.ranges_queue.qsize() > 10:
-                        break
-                self.trolled = True
+            elif self.ranges_queue.qsize() == 0:
+                ranges = self.get_unsearched_ranges()
+                for i in range(20):
+                    self.ranges_queue.put(
+                        ranges[i]
+                    )
                 search_range = self.ranges_queue.get()
             elif self.compressing:
                 logger.debug('Get smallest gap between two ranges')
                 search_range = self.get_unsearched_ranges()[0]
             else:
                 logger.debug(
-                    'Getting values between two random ranges weighted by gap size'
+                    'Getting values between two random ranges'
                 )
-                search_range = random.choices(
-                    self.get_unsearched_ranges(),
-                    weights=[1/x[0] for x in self.get_unsearched_ranges()]
-                )[0]
+                # search_range = random.choices(
+                #     self.get_unsearched_ranges(),
+                #     weights=[1/x[0] for x in self.get_unsearched_ranges()]
+                # )[0]
+                search_range = random.choice(self.get_unsearched_ranges())
             logger.debug(search_range)
             # Return the full range if gap is less than 60k
             if search_range[0] < self.chunk_size * 2:
@@ -128,7 +157,7 @@ class State(object):
                 end = search_range[1][1]
                 return (start, end)
             elif search_range[0] < self.chunk_size * 100:
-                # Get the value at middle - 60k
+                # Get the value at middle - self.chunk_size
                 range_start = int(max(
                     search_range[1][0] + (search_range[0] // 2) - (self.chunk_size // 2),
                     search_range[1][0]
